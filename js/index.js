ca6c804993684e8596b15f16800f8aba893aa6c0
ulisses malanski <87362996+malanski@users.noreply.github.com>
2022-01-17 22:26:23 -0300

    Add Image transition click

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ const imagemPrincipal = document.querySelector('.imagem-principal');
 
 imagemPrincipal.onclick = function() {
     let imgSrc = imagemPrincipal.getAttribute('src');
-    if(imgSrc === 'retrato-pontilhismo.png') {
-        imagemPrincipal.setAttribute('src', 'retrato-pontilhismo2.png')
+    if(imgSrc === 'retrato-pontilhismo2.png') {
+        imagemPrincipal.setAttribute('src', 'retrato-pontilhismo.png')
     } else {
-        imagemPrincipal.setAttribute('src', 'retrato-pontilhismo.png');
+        imagemPrincipal.setAttribute('src', 'retrato-pontilhismo2.png');
     }
 }
